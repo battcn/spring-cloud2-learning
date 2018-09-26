3fd9b985900a12a5a3482073b1a89e829e1df00b
Levin <1837307557@qq.com>
2018-09-26 11:47:42 +0800

    第三篇：注解式HTTP请求Feign（上）

---
+++ chapter3/order-server/src/main/java/com/battcn/api/ProductApi.java
@@ -0,0 +1,24 @@
+package com.battcn.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author Levin
+ * @since 2018/9/26 0026
+ */
+@FeignClient(name = "product-server/products")
+public interface ProductApi {
+
+    /**
+     * 根据产品ID查询产品信息
+     *
+     * @param productId ID
+     * @return 查询结果
+     */
+    @GetMapping("{product_id}")
+    String selectProductById(@PathVariable("product_id") Long productId);
+
+
+}
---
+++ chapter3/product-server/src/main/java/com/battcn/ProductApplication.java
@@ -0,0 +1,28 @@
+package com.battcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Levin
+ */
+@EnableEurekaClient
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+    @GetMapping("/products/{id}")
+    public String query(@PathVariable Long id) {
+        return id + "：Spring Boot...";
+    }
+}
