17c9c664be8a317ecbabd9b0e612e33b53351a97
Levin <1837307557@qq.com>
2018-09-26 11:10:12 +0800

    第一篇：认识Eureka

--- chapter1/eureka-client/src/main/java/com/battcn/MyEurekaClient.java
+++ chapter1/eureka-client/src/main/java/com/battcn/MyEurekaClient.java
@@ -3,10 +3,12 @@ package com.battcn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Levin
  */
+@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MyEurekaClient {
